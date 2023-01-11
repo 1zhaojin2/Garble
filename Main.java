607e055abf0938760b3cf2c5620f81e28e9ce37e
1zhaojin2 <1zhaojin2@hdsb.ca>
2023-01-11 12:13:52 -0500

    Hi

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
 import javax.swing.JFrame;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+
 public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
         frame.setTitle("Garble");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
         frame.setSize(600,600);
         frame.setVisible(true);
 
+        //sets the icon of the frame
+        ImageIcon image = new ImageIcon("image.png");
+        frame.setIconImage(image.getImage());
+
+        frame.getContentPane().setBackground(new Color(30,30,30));
     }
 }
